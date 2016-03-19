9b2aee693bf045130f941faa8a7d58b4b3c159aa
VMADAS00 <mvenky155@gmail.com>
2016-03-19 19:38:18 +0530

    Added files via upload

---
+++ Session_1_SumofTwoNumbers_Assignment_1.java
@@ -0,0 +1,12 @@
+
+public class Session_1_SumofTwoNumbers_Assignment_1 {
+	public static void main(String[] args) {
+		
+		int i=3;
+		double j=9.6676;
+		int k = i + (int) j;
+		System.out.println("result of two numbers one interger and one flot : " +k);
+
+	}
+	
+}
